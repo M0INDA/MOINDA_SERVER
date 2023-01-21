2d6f48e936a31d09d7c07fac7e3c12ec7680967e
jhMOON <113878993+JiHyeunM@users.noreply.github.com>
2023-01-22 04:28:22 +0900

    scripts

---
+++ scripts/gh_deploy.sh
@@ -0,0 +1,11 @@
+cd /home/ubuntu/github_action
+
+npm install
+
+npm install class-validator class-transformer mysql2 bcrypt @types/bcrypt
+
+npm run build
+
+sudo pm2 kill
+
+sudo NODE_ENV=ci pm2 start dist/apps/moinda-pd-api/main.js
