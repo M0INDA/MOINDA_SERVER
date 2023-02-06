5638568e33f54d1a72656cedcfdcec688188c598
JiHyeunM <moon011011@gmail.com>
2023-02-06 21:18:21 +0900

    코드 삭제

--- apps/moinda-pd-admin/src/main.ts
+++ apps/moinda-pd-admin/src/main.ts
@@ -3,19 +3,6 @@ import { AdminModule } from './admin.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AdminModule);
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: [
-      'access-control-allow-origin',
-      'X-Requested-With',
-      'Content-Type',
-      'Accept',
-    ],
-    credentials: true,
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  });
   await app.listen(3000);
 }
 bootstrap();
