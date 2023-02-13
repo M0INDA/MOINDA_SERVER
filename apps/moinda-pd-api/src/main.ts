8ce24734c0a0f88203a0e2806a5ede92a1cf7de6
1005jh <kjhhy1005@gmail.com>
2023-02-13 15:54:08 +0900

    credentials 추가

--- apps/moinda-pd-api/src/main.ts
+++ apps/moinda-pd-api/src/main.ts
@@ -4,7 +4,13 @@ import { ApiModule } from './api.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiModule, { cors: true });
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       //     whitelist: true, //decorator 없는 property object 거름
